9f256cdf328739d489da56438819c5bc6a6e0ee7
ryichk <ryichk@gmail.com>
2021-07-23 23:02:30 +0900

    :recycle: refactor not to use switch

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,33 +1,24 @@
 import React from 'react';
 
-import { ButtonProps } from './Button';
+import type { ButtonProps } from './Button';
+
+const SIZES = {
+  small: 'py-1 px-4 text-xs',
+  medium: 'py-1 px-5 text-base',
+  large: 'py-1 px-14 text-base',
+};
 
 export const Button: React.FunctionComponent<ButtonProps> = ({
   bgColor = 'bg-primary',
   size = 'medium',
   label,
   ...props
-}: ButtonProps) => {
-  let sizeClasses;
-  switch (size) {
-    case 'small':
-      sizeClasses = 'py-1 px-4 text-xs';
-      break;
-    case 'medium':
-      sizeClasses = 'py-1 px-5 text-base';
-      break;
-    case 'large':
-      sizeClasses = 'py-1 px-14 text-base';
-      break;
-  }
-
-  return (
-    <button
-      type='button'
-      className={`${bgColor} ${sizeClasses} text-white hover:bg-opacity-70 shadow-md rounded-3xl border border-gray-200`}
-      {...props}
-    >
-      {label}
-    </button>
-  );
-};
+}: ButtonProps) => (
+  <button
+    type='button'
+    className={`${bgColor} ${SIZES[size]} text-white hover:bg-opacity-70 shadow-md rounded-3xl border border-gray-200`}
+    {...props}
+  >
+    {label}
+  </button>
+);
