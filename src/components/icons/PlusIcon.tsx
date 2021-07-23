743e80e8418139ddd2093f1b4a758680be90c0e1
ryichk <ryichk@gmail.com>
2021-07-24 00:30:12 +0900

    :lipstick: Added SearchBox component

---
+++ src/components/icons/PlusIcon.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import type { IconProps } from './icon';
+
+export const PlusIcon: React.FC<IconProps> = (props: IconProps) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='h-6 w-6'
+    fill='none'
+    viewBox='0 0 24 24'
+    stroke={props.color || 'currentColor'}
+    width={props.size || 24}
+    height={props.size || 24}
+  >
+    <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M12 4v16m8-8H4' />
+  </svg>
+);
