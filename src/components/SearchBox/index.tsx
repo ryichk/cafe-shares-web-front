580cba50568ac3940f1a79ffd0aee6ffa64775c3
ryichk <ryichk@gmail.com>
2021-07-28 22:49:07 +0900

    :lipstick: Update SearchBox component styles

--- src/components/SearchBox/index.tsx
+++ src/components/SearchBox/index.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { PlusIcon, SearchIcon } from '../icons';
 
 export const SearchBox: React.FC = () => (
-  <div className='m-1 p-1.5 pr-4 max-w-sm mx-auto text-gray-500 border border-gray-200 rounded-md'>
+  <div className='flex justify-between py-1.5 pl-2 pr-3 max-w-sm mx-auto text-gray-500 bg-white border border-gray-200 rounded-md'>
     <SearchIcon classes='h-5 w-5 mt-0.5 text-gray-500' />
-    <span className='ml-5 text-sm'>検索条件を設定</span>
-    <span className='float-right'>
-      <PlusIcon classes='h-6 w-6 text-gray-500' />
-    </span>
+    <span className='-ml-48 pt-1 text-sm'>検索条件を設定</span>
+    <PlusIcon classes='h-6 w-6 text-gray-500' />
   </div>
 );
