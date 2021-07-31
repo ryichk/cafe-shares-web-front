0f4437d75c5f7e749c44ae015dee1830a8046709
ryichk <ryichk@gmail.com>
2021-07-31 23:10:55 +0900

    :wrench: Update daisyUI default theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,4 +14,37 @@ module.exports = {
     extend: {},
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        default: {
+          primary: '#81B29A',
+          'primary-focus': '#719885',
+          'primary-content': '#FFFFFF',
+
+          secondary: '#F2CC8F',
+          'secondary-focus': '#D5BA90',
+          'secondary-content': '#FFFFFF',
+
+          accent: '#E07A5F',
+          'accent-focus': '#C68270',
+          'accent-content': '#FFFFFF',
+
+          neutral: '#3D4451',
+          'neutral-focus': '#2A2E37',
+          'neutral-content': '#FFFFFF',
+
+          'base-100': '#F4F1DE',
+          'base-200': '#F9FAFB',
+          'base-300': '#D1D5DB',
+          'base-content': '#3D405B',
+
+          info: '#2094F3',
+          success: '#009485',
+          warning: '#FF9900',
+          error: '#FF5724',
+        },
+      },
+    ],
+  },
 };
