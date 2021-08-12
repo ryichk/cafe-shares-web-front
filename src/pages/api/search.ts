10076e44a19ef9997c9b41d2ff215f397876f87c
Ichiki Ryo <tohodaiichiki@gmail.com>
2021-08-12 19:25:23 +0900

    :art: Prettier formatted

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -5,10 +5,14 @@ const defaultEndPoint = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?
 const search = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   let URL = defaultEndPoint;
 
-  URL = typeof req.query.large_area === undefined ? URL : `${URL}&large_area=${req.query.largeArea}`;
+  URL =
+    typeof req.query.large_area === undefined ? URL : `${URL}&large_area=${req.query.largeArea}`;
   URL = typeof req.query.keyword === undefined ? URL : `${URL}&keyword=${req.query.keyword}`;
   URL = typeof req.query.wifi === undefined ? URL : `${URL}&wifi=${req.query.wifi}`;
-  URL = typeof req.query.privateRoom === undefined ? URL : `${URL}&private_room=${req.query.privateRoom}`;
+  URL =
+    typeof req.query.privateRoom === undefined
+      ? URL
+      : `${URL}&private_room=${req.query.privateRoom}`;
   URL = typeof req.query.noSmoking === undefined ? URL : `${URL}&no_smoking=${req.query.noSmoking}`;
   URL = typeof req.query.parking === undefined ? URL : `${URL}&parking=${req.query.parking}`;
   URL = typeof req.query.pet === undefined ? URL : `${URL}&pet=${req.query.pet}`;
