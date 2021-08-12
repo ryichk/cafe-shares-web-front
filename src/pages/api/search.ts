b7e30da30b9e8e9c4365fd637c6eb7cb504dc129
Ichiki Ryo <tohodaiichiki@gmail.com>
2021-08-12 19:17:28 +0900

    :recycle: Refactor if statement with ternary operator

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -5,36 +5,16 @@ const defaultEndPoint = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?
 const search = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   let URL = defaultEndPoint;
 
-  if (typeof req.query.large_area !== undefined) {
-    URL = `${URL}&large_area=${req.query.largeArea}`;
-  }
-  if (typeof req.query.keyword !== undefined) {
-    URL = `${URL}&keyword=${req.query.keyword}`;
-  }
-  if (typeof req.query.wifi !== undefined) {
-    URL = `${URL}&wifi=${req.query.wifi}`;
-  }
-  if (typeof req.query.privateRoom !== undefined) {
-    URL = `${URL}&private_room=${req.query.privateRoom}`;
-  }
-  if (typeof req.query.noSmoking !== undefined) {
-    URL = `${URL}&no_smoking=${req.query.noSmoking}`;
-  }
-  if (typeof req.query.parking !== undefined) {
-    URL = `${URL}&parking=${req.query.parking}`;
-  }
-  if (typeof req.query.pet !== undefined) {
-    URL = `${URL}&pet=${req.query.pet}`;
-  }
-  if (typeof req.query.card !== undefined) {
-    URL = `${URL}&card=${req.query.card}`;
-  }
-  if (typeof req.query.order !== undefined) {
-    URL = `${URL}&order=${req.query.order}`;
-  }
-  if (typeof req.query.start !== undefined) {
-    URL = `${URL}&start=${req.query.start}`;
-  }
+  URL = typeof req.query.large_area === undefined ? URL : `${URL}&large_area=${req.query.largeArea}`;
+  URL = typeof req.query.keyword === undefined ? URL : `${URL}&keyword=${req.query.keyword}`;
+  URL = typeof req.query.wifi === undefined ? URL : `${URL}&wifi=${req.query.wifi}`;
+  URL = typeof req.query.privateRoom === undefined ? URL : `${URL}&private_room=${req.query.privateRoom}`;
+  URL = typeof req.query.noSmoking === undefined ? URL : `${URL}&no_smoking=${req.query.noSmoking}`;
+  URL = typeof req.query.parking === undefined ? URL : `${URL}&parking=${req.query.parking}`;
+  URL = typeof req.query.pet === undefined ? URL : `${URL}&pet=${req.query.pet}`;
+  URL = typeof req.query.card === undefined ? URL : `${URL}&card=${req.query.card}`;
+  URL = typeof req.query.order === undefined ? URL : `${URL}&order=${req.query.order}`;
+  URL = typeof req.query.start === undefined ? URL : `${URL}&start=${req.query.start}`;
 
   const result = await fetch(encodeURI(URL));
   res.json(result.body);
