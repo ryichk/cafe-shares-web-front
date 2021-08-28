3d0ed51095ef754ed0a3ac8d7a1480abc2c28cb4
ryichk <ryichk@gmail.com>
2021-08-29 04:49:11 +0900

    :sparkles: Added favicon and ogp setting

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { Card } from './CardList/Card';
 export { CardList } from './CardList';
 export { SearchModal } from './SearchModal';
 export { ScrollToTop } from './ScrollToTop';
+export { SEO } from './layouts/SEO';
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+import { SEO } from '../components';
 import { GA_TRACKING_ID } from 'lib/gtag';
 
 export default class MyDocument extends Document {
@@ -6,6 +8,15 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <SEO />
+          <link rel='apple-touch-icon' sizes='180x180' href='/favicons/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/favicons/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/favicons/favicon-16x16.png' />
+          <link rel='manifest' href='/favicons/site.webmanifest' />
+          <link rel='mask-icon' href='/favicons/safari-pinned-tab.svg' color='#000000' />
+          <meta name='msapplication-TileColor' content='#ffffff' />
+          <meta name='msapplication-config' content='/favicons/browserconfig.xml' />
+          <meta name='theme-color' content='#719885' />
           {
             <>
               <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
