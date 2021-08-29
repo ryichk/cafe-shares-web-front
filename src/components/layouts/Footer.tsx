d8a03ece08f4d2ffd33888c512e70be6f228db91
ryichk <ryichk@gmail.com>
2021-08-29 20:03:22 +0900

    :sparkles: add target blank to link

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -4,7 +4,12 @@ export const Footer: VFC = () => (
   <footer className='flex justify-center p-5 bg-base-content text-white'>
     <p>
       © 2021{' '}
-      <a href='https://github.com/ryichk' className='link'>
+      <a
+        href='https://github.com/ryichk'
+        target='_blank'
+        rel='noopener noreferrer'
+        className='link'
+      >
         ryichk
       </a>
     </p>
