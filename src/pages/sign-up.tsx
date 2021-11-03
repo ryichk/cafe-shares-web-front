70bfed3567dcb2abbb16a1f6f21d83939bf064b5
ryichk <ryichk@gmail.com>
2021-11-03 21:31:51 +0900

    :lipstick: Added underline to input field

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -141,47 +141,8 @@ const SignUp: NextPage = () => {
       {isSubmitted ? (
         <div className='card bg-white shadow-2xl w-96 p-10 m-auto sm:mt-10'>
           <div className='form-control'>
-            <div className='flex m-1 p-2'>
-              <label className='label'>
-                <UserIcon classes='h-6 w-6 text-primary' />
-              </label>
-              <input
-                name='username'
-                type='text'
-                placeholder='username'
-                className='bg-white ml-1 p-2'
-                value={username}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className='flex m-1 mb-10 p-2'>
-              <label className='label'>
-                <MessageIcon classes='h-6 w-6 text-primary' />
-              </label>
-              <input
-                name='code'
-                type='text'
-                placeholder='verification code'
-                className='bg-white ml-1 p-2'
-                value={code}
-                onChange={handleInputChange}
-              />
-            </div>
-          </div>
-          <Button size='large' label='activate account' onClick={confirmSignUp} />
-        </div>
-      ) : (
-        <>
-          <div className='card bg-white shadow-2xl w-96 p-10 m-auto sm:mt-10'>
-            <div className='form-control'>
-              <div className='mb-20'>
-                <Link href='/'>
-                  <a>
-                    <ArrowLeftIcon classes='h-5 w-5' />
-                  </a>
-                </Link>
-              </div>
-              <div className='flex m-1 p-2'>
+            <div className='m-1 p-2'>
+              <div className='flex border-b border-primary'>
                 <label className='label'>
                   <UserIcon classes='h-6 w-6 text-primary' />
                 </label>
@@ -194,43 +155,96 @@ const SignUp: NextPage = () => {
                   onChange={handleInputChange}
                 />
               </div>
-              <div className='flex m-1 p-2'>
+            </div>
+            <div className='m-1 p-2'>
+              <div className='flex border-b border-primary'>
                 <label className='label'>
-                  <MailIcon classes='h-6 w-6 text-primary' />
+                  <MessageIcon classes='h-6 w-6 text-primary' />
                 </label>
                 <input
-                  name='email'
-                  type='email'
-                  placeholder='user@email.com'
+                  name='code'
+                  type='text'
+                  placeholder='verification code'
                   className='bg-white ml-1 p-2'
-                  value={email}
+                  value={code}
                   onChange={handleInputChange}
                 />
-              </div>
-              <div className='flex m-1 p-2'>
-                <label className='label'>
-                  <LockIcon classes='h-6 w-6 text-primary' />
-                </label>
-                <input
-                  name='password'
-                  type={isMasked ? 'password' : 'text'}
-                  placeholder='password'
-                  className='bg-white ml-1 p-2'
-                  value={password}
-                  onChange={handleInputChange}
-                />
-                {isMasked ? (
-                  <div onClick={() => setIsMasked(false)}>
-                    <EyeOffIcon classes='m-2 text-secondary' />
-                  </div>
-                ) : (
-                  <div onClick={() => setIsMasked(true)}>
-                    <EyeIcon classes='m-2' />
-                  </div>
-                )}
               </div>
             </div>
-            <Button size='large' label='create user' onClick={signUp} />
+          </div>
+          <div className='mt-5 mx-auto'>
+              <Button size='large' label='activate account' onClick={confirmSignUp} />
+            </div>
+        </div>
+      ) : (
+        <>
+          <div className='card bg-white shadow-2xl w-96 p-10 m-auto sm:mt-10'>
+            <div className='form-control'>
+              <div className='mb-20'>
+                <Link href='/'>
+                  <a>
+                    <ArrowLeftIcon classes='h-5 w-5' />
+                  </a>
+                </Link>
+              </div>
+              <div className='m-1 p-2'>
+                <div className='flex border-b border-primary'>
+                  <label className='label'>
+                    <UserIcon classes='h-6 w-6 text-primary' />
+                  </label>
+                  <input
+                    name='username'
+                    type='text'
+                    placeholder='username'
+                    className='bg-white ml-1 p-2'
+                    value={username}
+                    onChange={handleInputChange}
+                  />
+                </div>
+              </div>
+              <div className='m-1 p-2'>
+                <div className='flex border-b border-primary'>
+                  <label className='label'>
+                    <MailIcon classes='h-6 w-6 text-primary' />
+                  </label>
+                  <input
+                    name='email'
+                    type='email'
+                    placeholder='user@email.com'
+                    className='bg-white ml-1 p-2'
+                    value={email}
+                    onChange={handleInputChange}
+                  />
+                </div>
+              </div>
+              <div className='m-1 p-2'>
+                <div className='flex border-b border-primary'>
+                  <label className='label'>
+                    <LockIcon classes='h-6 w-6 text-primary' />
+                  </label>
+                  <input
+                    name='password'
+                    type={isMasked ? 'password' : 'text'}
+                    placeholder='password'
+                    className='bg-white ml-1 p-2'
+                    value={password}
+                    onChange={handleInputChange}
+                  />
+                  {isMasked ? (
+                    <div onClick={() => setIsMasked(false)}>
+                      <EyeOffIcon classes='m-2 text-secondary' />
+                    </div>
+                  ) : (
+                    <div onClick={() => setIsMasked(true)}>
+                      <EyeIcon classes='m-2' />
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className='mt-5 mx-auto'>
+              <Button size='large' label='create user' onClick={signUp} />
+            </div>
           </div>
           <div className='mt-10 text-center'>
             <p>Already have an account?</p>
