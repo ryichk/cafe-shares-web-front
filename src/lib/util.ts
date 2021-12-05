2cb468a5264325455721c30ae3c2e3048e7ab211
ryichk <ryichk@gmail.com>
2021-12-05 18:46:12 +0900

    :sparkles: Added Search Result Page

---
+++ src/lib/util.ts
@@ -0,0 +1,33 @@
+import { SearchKey } from '../interfaces';
+
+export const convertUrlParamsToJSON = (query: string): Record<SearchKey, string> => {
+  if (
+    query.includes('start') ||
+    query.includes('largeArea') ||
+    query.includes('keyword') ||
+    query.includes('wifi') ||
+    query.includes('privateRoom') ||
+    query.includes('noSmoking') ||
+    query.includes('parking') ||
+    query.includes('pet') ||
+    query.includes('card') ||
+    query.includes('order')
+  ) {
+    return JSON.parse(
+      `{"${decodeURI(query).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"')}"}`,
+    );
+  } else {
+    return {
+      start: '1',
+      largeArea: 'Z011',
+      keyword: '',
+      wifi: '0',
+      privateRoom: '0',
+      noSmoking: '0',
+      parking: '0',
+      pet: '0',
+      card: '0',
+      order: '4',
+    };
+  }
+};
