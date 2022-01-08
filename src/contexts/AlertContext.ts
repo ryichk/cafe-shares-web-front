598331d22026bcab524da36134fcaa488714c6a1
ryichk <ryichk@gmail.com>
2022-01-08 17:36:42 +0900

    :lipstick: Added InfoAlert component

--- src/contexts/AlertContext.ts
+++ src/contexts/AlertContext.ts
@@ -3,10 +3,14 @@ import { createContext, Dispatch, SetStateAction } from 'react';
 interface IAlertContext {
   isError: boolean;
   errorMessage: string;
+  isInfo: boolean;
+  infoMessage: string;
   isSuccess: boolean;
   successMessage: string;
   setIsError: Dispatch<SetStateAction<boolean>>;
   setErrorMessage: Dispatch<SetStateAction<string>>;
+  setIsInfo: Dispatch<SetStateAction<boolean>>;
+  setInfoMessage: Dispatch<SetStateAction<string>>;
   setIsSuccess: Dispatch<SetStateAction<boolean>>;
   setSuccessMessage: Dispatch<SetStateAction<string>>;
 }
@@ -14,10 +18,14 @@ interface IAlertContext {
 const initialContext: IAlertContext = {
   isError: false,
   errorMessage: '',
+  isInfo: false,
+  infoMessage: '',
   isSuccess: false,
   successMessage: '',
   setIsError: () => {},
   setErrorMessage: () => {},
+  setIsInfo: () => {},
+  setInfoMessage: () => {},
   setIsSuccess: () => {},
   setSuccessMessage: () => {},
 };
